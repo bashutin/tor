f1ff56a7f8fb61a168f893af555aa481b49938e6
Vladimir <bashutin.v@gmail.com>
2023-03-02 10:42:51 +0300

    Update transparent_proxy.sh

--- transparent_proxy.sh
+++ transparent_proxy.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # Add to /etc/tor/torrc
-#VirtualAddrNetworkIPv4 10.192.0.0/10
-#AutomapHostsOnResolve 1
-#TransPort 192.168.50.111:9040
-#DNSPort 192.168.50.111:5353
+#TransPort 0.0.0.0:9040
+#DNSPort 0.0.0.0:5354
 
-# Tor's TransPort
-_trans_port="9040"
+# Tor's Ports
+trans_port="9040"
+dns_port='5354
 
 # your internal interface
 _inc_if="enp9s0"
@@ -15,7 +14,7 @@ _inc_if="enp9s0"
 iptables -F
 iptables -t nat -F
 
-iptables -t nat -A PREROUTING -i $_inc_if -p udp --dport 53 -j REDIRECT --to-ports 5353
-iptables -t nat -A PREROUTING -i $_inc_if -p udp --dport 5353 -j REDIRECT --to-ports 5353
+iptables -t nat -A PREROUTING -i $_inc_if -p udp --dport 53 -j REDIRECT --to-ports $dns_port
+iptables -t nat -A PREROUTING -i $_inc_if -p udp --dport 5353 -j REDIRECT --to-ports $dns_port
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -t nat -A PREROUTING -i $_inc_if -p tcp --syn -j REDIRECT --to-ports $_trans_port
+iptables -t nat -A PREROUTING -i $_inc_if -p tcp --syn -j REDIRECT --to-ports $trans_port
